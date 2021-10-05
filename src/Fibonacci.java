37998cb15a89c650d5d1f0d638efbfdabab9910e
devajagan <90674727+devajagan@users.noreply.github.com>
2021-10-05 15:02:47 +0530

    first commit

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,2 +1,21 @@
-public class Fibonacci {
-}
+
+    import java.util.*;
+    public class Fibonacci
+    {
+        public static void main(String[] args)
+        {
+            Scanner sc=new Scanner(System.in);
+            int t1 = 0, t2 = 1;
+            System.out.print("Enter the number of terms: ");
+            int n=sc.nextInt();
+            System.out.println("First " + n + " terms of fibonnaci series: ");
+            for (int i = 1; i <= n; ++i)
+            {
+                System.out.print(t1 + " ");
+                int sum = t1 + t2;
+                t1 = t2;
+                t2 = sum;
+            }
+        }
+    }
+
